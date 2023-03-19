33bea9579bfd98845ced2ea115c0444fe4ae5009
Oleksandr Cherevkov <oleksandrcherevkov@gmail.com>
2023-03-19 20:18:06 +0200

    add bike status

--- CleanTemplate.Domain/Entities/Bike.cs
+++ CleanTemplate.Domain/Entities/Bike.cs
@@ -1,9 +1,11 @@
+using CleanTemplate.Domain.Enums;
 using NetTopologySuite.Geometries;
 
 namespace CleanTemplate.Domain.Entities;
 
 public class Bike
 {
+    private static string StatusMissmatchExceptionMessage = "Bike cannot be transferred to provided status. Current: {0}, New: {1}";
     public Bike()
     {
         _id = Guid.NewGuid().ToString();
@@ -13,6 +15,52 @@ public class Bike
     public string Id => _id;
     public Point Location { get; set; }
     public int? CurrentStationId { get; set; }
+    private BikeStatus _status = BikeStatus.Repairing;
+    public BikeStatus Status
+    {
+        get => _status;
+        set
+        {
+            switch (_status)
+            {
+                case BikeStatus.Active:
+                    switch (value)
+                    {
+                        case BikeStatus.Rented:
+                        case BikeStatus.Repairing:
+                            break;
+                        default:
+                            throw new ArgumentException(string.Format(StatusMissmatchExceptionMessage, _status, value));
+                    }
+                    break;
+                case BikeStatus.Rented:
+                    switch (value)
+                    {
+                        case BikeStatus.Active:
+                        case BikeStatus.Repairing:
+                            break;
+                        default:
+                            throw new ArgumentException(string.Format(StatusMissmatchExceptionMessage, _status, value));
+                    }
+                    break;
+                case BikeStatus.Repairing:
+                    switch (value)
+                    {
+                        case BikeStatus.Active:
+                        case BikeStatus.Broken:
+                            break;
+                        default:
+                            throw new ArgumentException(string.Format(StatusMissmatchExceptionMessage, _status, value));
+                    }
+                    break;
+                case BikeStatus.Broken:
+                    throw new ArgumentException(string.Format(StatusMissmatchExceptionMessage, _status, value));
+                default:
+                    throw new Exception($"Unknown bike status: {_status}");
+            }
+            _status = value;
+        }
+    }
     public Station? CurrentStation { get; set; }
     public ICollection<Ticket> Tickets { get; } = new List<Ticket>();
 }
---
+++ CleanTemplate.Domain/Enums/BikeStatusEnum.cs
@@ -0,0 +1,9 @@
+namespace CleanTemplate.Domain.Enums;
+
+public enum BikeStatus
+{
+    Active,
+    Rented,
+    Repairing,
+    Broken,
+}
