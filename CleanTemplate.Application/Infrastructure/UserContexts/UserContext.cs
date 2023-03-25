34af9c13711bae6abaa45f496db06a29f51c85c1
Oleksandr Cherevkov <oleksandrcherevkov@gmail.com>
2023-03-25 18:26:25 +0200

    add id to user context

--- CleanTemplate.Application/Infrastructure/UserContexts/UserContext.cs
+++ CleanTemplate.Application/Infrastructure/UserContexts/UserContext.cs
@@ -3,6 +3,7 @@ using CleanTemplate.Domain.Enums;
 namespace CleanTemplate.Application.Infrastructure.UserContexts;
 public class UserContext
 {
+    public string Id { get; set; }
     public string Email { get; set; }
     public RoleEnum Role { get; set; }
 }
