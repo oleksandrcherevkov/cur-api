97111a0d0bcd85825ffc72ac8faf839398f334f6
Oleksandr Cherevkov <oleksandrcherevkov@gmail.com>
2023-03-26 12:45:16 +0300

    add services

--- CleanTemplate.WebApi/Program.cs
+++ CleanTemplate.WebApi/Program.cs
@@ -1,10 +1,15 @@
 using System.Text;
+using CleanTemplate.Application.Bikes;
 using CleanTemplate.Application.Infrastructure.UserContexts;
 using CleanTemplate.Application.Infrastructurel;
+using CleanTemplate.Application.Stations;
+using CleanTemplate.Application.Tickets;
+using CleanTemplate.Application.Users;
 using CleanTemplate.Persistence;
 using CleanTemplate.WebApi.Infrastructure.Data.Extensions;
 using CleanTemplate.WebApi.Infrastructure.ExceptionsHandling.Extensions;
 using CleanTemplate.WebApi.Infrastructure.Jwt.Extentions;
+using CleanTemplate.WebApi.Infrastructure.Seeding.Extentions;
 using CleanTemplate.WebApi.Infrastructure.UserContexts.Extensions;
 using FluentValidation;
 using MediatR;
@@ -56,6 +61,7 @@ builder.Services.AddAuthentication(options =>
         {
             ValidateIssuer = true,
             ValidateAudience = true,
+            ValidateIssuerSigningKey = true,
             ValidAudience = configuration["Jwt:Audience"],
             ValidIssuer = configuration["Jwt:Issuer"],
             IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Secret"])),
@@ -65,9 +71,16 @@ builder.Services.AddAuthentication(options =>
 // UserContext
 builder.Services.AddScoped<UserContext>();
 
+// Services registration
+builder.Services.AddScoped<BikesService>();
+builder.Services.AddScoped<StationsService>();
+builder.Services.AddScoped<TicketsService>();
+builder.Services.AddScoped<UsersService>();
+
 var app = builder.Build();
 
 await app.SetupDatabaseAsync();
+app.SeedDatabase();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
